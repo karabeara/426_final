4bbcb5e70e64ab8d4a86a2db49765aabde4944a8
Kara <karab@princeton.edu>
2017-05-15 20:26:21 -0400

    it kinda works!

---
+++ weirdKhaled/objects/face.js
@@ -0,0 +1,27 @@
+{
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "face.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 4,
+        "faces"         : 1,
+        "normals"       : 0,
+        "colors"        : 0,
+        "uvs"           : 0,
+        "materials"     : 0
+    },
+    "scale" : 1.000000,
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+    "vertices": [-1.000000,-1.000000,0.000000,-1.000000,1.000000,0.000000,1.000000,1.000000,0.000000,1.000000,-1.000000,0.000000],
+    "morphTargets": [],
+    "morphColors": [],
+    "normals": [],
+    "colors": [],
+    "uvs": [[]],
+    "faces": [3,0,1,2,3,0]
+}
